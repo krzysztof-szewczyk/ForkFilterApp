4a6357042004f9ba702c4bed949f255b4c537910
Krzysztof Szewczyk <krzysieksz96@gmail.com>
2019-04-11 14:37:33 +0200

    gui update, background, components

--- src/Controller/WindowController.java
+++ src/Controller/WindowController.java
@@ -2,6 +2,8 @@ package Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class WindowController {
@@ -11,12 +13,22 @@ public class WindowController {
     private Button loadBtn;
 
     @FXML
+    private TextField newFilterSizeTxt;
+
+    @FXML
     private GridPane matrixGridPane;
+
+    @FXML
+    private ChoiceBox<String> filterChoiceBox;
 
     @FXML
     public void initialize(){
 
         loadBtn.setText("Load");
+        newFilterSizeTxt.setText("3");
+        filterChoiceBox.getItems().setAll("Custom","Filter1","Filter2","Filter3");
+        filterChoiceBox.getSelectionModel().selectFirst();
+
 
     }
 }
